c30f06fd9519b7329239a11f2366d915462e6978
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2021-02-19 15:56:34 +0100

    added spatie package

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Hash;
+
 
 class UserSeeder extends Seeder
 {
@@ -15,6 +18,33 @@ class UserSeeder extends Seeder
     public function run()
     {
         //
-        User::factory()->count(10)->create();
+        // Reset cached roles and permissions
+        app()['cache']->forget('spatie.permission.cache');
+
+
+        Role::create(['name' => 'user']);
+        $user = User::create([
+            'name' => 'user',
+            'email' => 'user@example.com',
+            'password' => Hash::make('password'),
+        ]);
+        $user->assignRole('user');
+
+        Role::create(['name' => 'advertiser']);
+        $advertiser = User::create([
+            'name' => 'advertiser',
+            'email' => 'advertiser@example.com',
+            'password' => Hash::make('password'),
+        ]);
+        $advertiser->assignRole('advertiser');
+
+        Role::create(['name' => 'admin']);
+        $admin = User::create([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+        ]);
+        $admin->assignRole('admin');
+        $admin->assignRole('advertiser');
     }
 }
